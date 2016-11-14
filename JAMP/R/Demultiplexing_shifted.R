ca0dcfc8b25dd93840893dce27ab28f8723fd78b
Vasco Elbrecht <luckylion07@googlemail.com>
2016-11-14 08:22:46 +0100

    demulti

--- JAMP/R/Demultiplexing_shifted.R
+++ JAMP/R/Demultiplexing_shifted.R
@@ -4,7 +4,9 @@ Demultiplexing_shifted <- function(file1, file2, tags=NA, combinations=NA){
 
 Core(module="Demultiplexing_shifted")
 
-readL <- 251
+# get file compression
+compression <- gsub(".*\\.(.*)$", "\\1", file1)
+
 
 # get tagging table
 if(tags=="BF_BR"){
@@ -16,18 +18,21 @@ combos <- read.csv(combinations, stringsAsFactors=F)
 
 savesequ <- function(file_name1="N_debres_r1.txt", file_name2="N_debres_r2.txt", rm1=0, rm2=0){
 if(length(A)>0){
+
 rm1 <- rm1+1 # remove nucleotides
 rm2 <- rm2+1
 exp_sequ1 <- data1[sort(c(A*4, A*4-1, A*4-2, A*4-3))]
+readL <- nchar(exp_sequ1[seq(2, length(exp_sequ1), 4)])
 exp_sequ1[seq(2, length(exp_sequ1), 4)] <- substr(exp_sequ1[seq(2, length(exp_sequ1), 4)], rm1, readL)
 exp_sequ1[seq(4, length(exp_sequ1), 4)] <- substr(exp_sequ1[seq(4, length(exp_sequ1), 4)], rm1, readL)
 
 exp_sequ2 <- data2[sort(c(A*4, A*4-1, A*4-2, A*4-3))]
+readL <- nchar(exp_sequ2[seq(2, length(exp_sequ2), 4)])
 exp_sequ2[seq(2, length(exp_sequ2), 4)] <- substr(exp_sequ2[seq(2, length(exp_sequ2), 4)], rm2, readL)
 exp_sequ2[seq(4, length(exp_sequ2), 4)] <- substr(exp_sequ2[seq(4, length(exp_sequ2), 4)], rm2, readL)
 
-cat(exp_sequ1, file= file_name1, append=T, sep="\n")
-cat(exp_sequ2, file= file_name2, append=T, sep="\n")}
+cat(exp_sequ1, file= paste("_data/", file_name1, sep=""), append=T, sep="\n")
+cat(exp_sequ2, file= paste("_data/", file_name2, sep=""), append=T, sep="\n")}
 }
 # save function end
 
@@ -36,14 +41,25 @@ chunk <- 100000*4
 
 for (i in 1:length(file1)){
 
-con1 <- gzfile(file1[i], "rt") # read stuff
-con2 <- gzfile(file2[i], "rt") # read stuff
-
+if(compression=="gz"){
+con1 <- gzfile(file1[i], "rt")
+con2 <- gzfile(file2[i], "rt")
+} else if(compression=="bz2"){
+con1 <- bzfile(file1[i], "rt")
+con2 <- bzfile(file2[i], "rt")
+} else if(compression=="fastq"){
+con1 <- file(ngsfile1, "rt")
+con2 <- file(ngsfile2, "rt")
+} else {
+warning("Raw data has to be compressed in \".gz\" or \".bz2\" or uncompressed in \".fastq\" format! Processing was stopped, please check your raw data format.")
+setwd("../")
+stop()
+}
 
 mytime <- Sys.time()
 repeat {
-	data1 <- readLines(con1, chunk) # read chunck
-	data2 <- readLines(con2, chunk) # read chunck
+data1 <- readLines(con1, chunk) # read chunck
+data2 <- readLines(con2, chunk) # read chunck
 
 data1 <- data1[!is.na(data1)] # remove NA in debres if end of file is reached!
 data2 <- data2[!is.na(data2)]
@@ -52,7 +68,6 @@ if (!length(data1)) break
 
 sequ1 <- data1[seq(2, length(data1), 4)]
 sequ2 <- data2[seq(2, length(data2), 4)]
-
 
 sequ1 <- substr(sequ1, 1, 5)
 sequ2 <- substr(sequ2, 1, 5)
@@ -66,25 +81,20 @@ tags <- paste(barcodeID[match_sequ1+1], barcodeID[match_sequ2+1], sep="_")
 match_sequ <- match(tags, combos$ID)
 
 
+# write down number of bases to remove
+match_sequ1[match_sequ1==0] <- NA
+rm1_master <- barcodes$rm[match_sequ1]
 
+match_sequ2[match_sequ2==0] <- NA
+rm2_master <- barcodes$rm[match_sequ1]
 
-glumanda <- barcodes[which(!duplicated(barcodes$ID)),] # isolate RM information from barcode file!
-
-
-rm1 <- sub("(B...)_.*", "\\1", combos$ID)
-rm2 <- sub("B..._(....)*", "\\1", combos$ID)
-
-rm1 <- as.numeric(glumanda$rm[match(rm1, glumanda$ID)])
-rm2 <- as.numeric(glumanda$rm[match(rm2, glumanda$ID)])
-
-#data.frame(combos$ID, rm1, rm2)
 
 for (k in 1:nrow(combos)){ # save all xxx tag combinations based on demultifile (name file)
 A <- which(match_sequ==k)
-if(length(A)>0){savesequ(combos$File1[k], combos$File2[k], rm1[k], rm2[k])}
+if(length(A)>0){savesequ(combos$File1[k], combos$File2[k], rm1_master[A], rm1_master[A])}
 }
 A <- which(is.na(match_sequ))
-if(length(A)>0){savesequ("N_debres_r1.txt", "N_debres_r2.txt", 0, 0)}
+if(length(A)>0){savesequ("N_debris_r1.txt", "N_debris_r2.txt", 0, 0)}
 
 # repeat loop enede
 } # looping all sequence files!
