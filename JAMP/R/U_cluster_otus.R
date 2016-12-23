e38c1798def113a6ccb13f4584bfc7756707561b
Vasco Elbrecht <luckylion07@googlemail.com>
2016-12-23 06:16:18 +0100

    added unoise2 to OTU clustering

--- JAMP/R/U_cluster_otus.R
+++ JAMP/R/U_cluster_otus.R
@@ -1,6 +1,6 @@
 # U_cluster_otus v0.1
 
-U_cluster_otus <- function(files="latest", minuniquesize=2, otu_radius_pct=3, strand="plus", filter=0.01, filterN=1){
+U_cluster_otus <- function(files="latest", minuniquesize=2, otu_radius_pct=3, strand="plus", filter=0.01, filterN=1, unoise_min=NA){
 
 Core(module="U_cluster_otus")
 cat(file="../log.txt", c("Version v0.1", "\n"), append=T, sep="\n")
@@ -34,6 +34,39 @@ cat(file="_stats/1_derep_logs.txt", meep, A, "\n", append=T, sep="\n")
 message(meep)
 }
 
+# unoise filtering of individual samples
+if (!is.na(unoise_min)){
+dir.create("_data/1_derep_unoise2")
+
+# change names
+denoised_names <- new_names
+denoised_names <- sub("1_derep_inc_singletons", "1_derep_unoise2", denoised_names)
+denoised_names <- sub(".fasta", "_unoise2.fasta", denoised_names)
+
+cmd <- paste("-unoise2 ", new_names, " -fastaout ", denoised_names, " -minampsize ", unoise_min, sep="")
+
+temp <- paste("\nDenoising ", length(cmd), " files using unoise2 wiht a minimum cluster size of ", unoise_min, ":", sep="")
+message(temp)
+cat(file="../log.txt", "\n", temp, append=T, sep="")
+
+
+for(i in 1:length(cmd)){
+A <- system2("usearch", cmd[i], stdout=T, stderr=T)
+
+A <- c(paste("usearch", cmd[i], sep=" "), A, "\n\n\n")
+cat(A, file="_stats/1_unoise2_logs.txt", sep="\n", append=T)
+
+temp <- paste(sub("_data/1_derep_unoise2/(.*)_PE_derep_unoise2.fasta", "\\1", denoised_names[i]), ": ", sub(".*100.0% (.*) good.*", "\\1 Amplicons keept", A[grep("100.0% .* good", A)]), sep="")
+message(temp)
+cat(file="../log.txt", "\n", temp, append=T, sep="")
+
+}
+
+new_names <- denoised_names # use denoised data for OTU clustering
+
+} # end unoise
+
+
 # 2 make OTUs!
 # merge all files into one
 
@@ -66,7 +99,7 @@ temp <- paste("Total number of sequences (not dereplicated): ", sub(".*nt in (.*
 message(temp)
 cat(file="../log.txt", temp, append=T, sep="\n")
 
-temp <- paste("United sequences are dereplicated with minuniquesize = ", minuniquesize , " into a total of ", sub("(.*)uniques written.*", "\\1", A[grep("uniques written", A)]), "unique sequences.", "\n", "File prepared for OTU clustering: \"", filename_all_unique, "\"", sep="")
+temp <- paste("United sequences are dereplicated with minuniquesize = ", minuniquesize , " into a total of ", sub("(.*) unique sequences.*", "\\1", A[grep(" unique sequences", A)]), " unique sequences.", "\n", "File prepared for OTU clustering: \"", filename_all_unique, "\"", sep="")
 message(temp)
 cat(file="../log.txt", temp, append=T, sep="\n")
 
@@ -95,7 +128,8 @@ dir.create("_data/3_Compare_OTU_derep/")
 dir.create("_stats/3_Compare_OTU_derep/")
 
 blast_names <- sub("1_derep_inc_singletons", "3_Compare_OTU_derep", new_names)
-blast_names <- sub("_PE_derep.fasta", ".txt", blast_names)
+blast_names <- sub("1_derep_unoise2", "3_Compare_OTU_derep", blast_names)
+blast_names <- sub("_PE_derep.*.fasta", ".txt", blast_names)
 log_names <- sub("_data", "_stats", blast_names)
 
 
@@ -112,7 +146,7 @@ for (i in 1:length(cmd)){
 A <- system2("usearch", cmd[i], stdout=T, stderr=T)
 cat(file= log_names[i], paste("usearch ", cmd[i], sep=""), "\n", A, append=F, sep="\n")
 
-meep <- sub(".*singletons/(.*)", "\\1", temp[i])
+meep <- sub("_data/.*/(.*)", "\\1", temp[i])
 pass <- sub(".*, (.*)% matched\r", "\\1", A[grep("matched\r", A)])
 exp <- rbind(exp, c(meep, pass))
 glumanda <- paste(meep," - ", pass, "% reads matched", sep="")
@@ -225,9 +259,10 @@ write.fasta(as.list(exp$sequ[-nrow(exp)]), exp$ID[-nrow(exp)], file.out=OTU_sub_
 dir.create("_data/5_subset/usearch_global")
 dir.create("_stats/5_subset/")
 
-new_names <- list.files("_data/1_derep_inc_singletons", full.names=T)
-blast_names <- sub("_PE_derep.fasta", ".txt", new_names)
+#new_names <- list.files("_data/1_derep_inc_singletons", full.names=T)
+blast_names <- sub("_PE_derep.*.fasta", ".txt", new_names)
 blast_names <- sub("1_derep_inc_singletons", "5_subset/usearch_global", blast_names)
+blast_names <- sub("1_derep_unoise2", "5_subset/usearch_global", blast_names)
 log_names <- sub("_data/", "_stats/", blast_names)
 log_names <- sub("/usearch_global", "", log_names)
 
