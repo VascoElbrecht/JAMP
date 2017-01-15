b43e0765bcf28b7324636e9b5d2a331337316571
Vasco Elbrecht <luckylion07@googlemail.com>
2017-01-15 17:47:41 +0100

    Bold tax - table generation

--- JAMP/R/BOLD_taxonomy.R
+++ JAMP/R/BOLD_taxonomy.R
@@ -47,15 +47,21 @@ message(i)
 
 }
 
-
-i <- 1
+i <- 41
 new_taxonomy <- NULL
 for (i in 1:length(temp_files)){
 
+if(readLines(paste("_stats/", data$ID[i], ".csv", collapse="", sep=""))[1]=="\"\""){new_taxonomy <- rbind(new_taxonomy, c(temp_files[i] , NA, NA, NA, NA, NA))} else {#no hit
+
+
+
 data_bold <- read.csv(paste("_stats/", temp_files[i], sep=""), sep="\t", stringsAsFactors=F)
 
+if(is.null(data_bold$family)){data_bold <- data.frame(data_bold, "family"=rep(NA, nrow(data_bold)))}
+if(is.null(data_bold$genus)){data_bold <- data.frame(data_bold, "genus"=rep(NA, nrow(data_bold)))}
+if(is.null(data_bold$species)){data_bold <- data.frame(data_bold, "species"=rep(NA, nrow(data_bold)))}
+
 data_bold <- data.frame("ID"=data_bold$ID, "taxitenti"=data_bold$taxonomicidentification, "similarity"=data_bold$similarity, "order"=data_bold$order, "family"=data_bold$family, "genus"=data_bold$genus, "species"=data_bold$species, stringsAsFactors=F)
-#data_bold <- data_bold[order(data_bold$similarity, decreasing=T),]
 
 data_bold$species[grep("sp.", data_bold$species)] <- data_bold$species[grep("sp.", data_bold$species)] <- NA
 
@@ -69,17 +75,17 @@ data_bold[,4:7][is.na(data_bold[,4:7])] <- "NA"
 # species
 tab_species <- as.data.frame(table(data_bold$species[data_bold$similarity>= MM[1]]), stringsAsFactors=F)
 tab_species <- tab_species[order(tab_species$Freq, decreasing=T),]
-adj_species <- paste(tab_species[1,1], " ", tab_species$Freq[1], "/", sum(tab_species$Freq), sep="")
+if(length(tab_species)==0){adj_species <- "NA"} else {adj_species <- paste(tab_species[1,1], " ", tab_species$Freq[1], "/", sum(tab_species$Freq), sep="")}
 
 # genus
 tab_genus <- as.data.frame(table(data_bold$genus[data_bold$similarity>= MM[2]]), stringsAsFactors=F)
 tab_genus <- tab_genus[order(tab_genus$Freq, decreasing=T),]
-adj_genus <- paste(tab_genus[1,1], " ", tab_genus$Freq[1], "/", sum(tab_genus$Freq), sep="")
+if(length(tab_genus)==0){adj_genus <- "NA"} else {adj_genus <- paste(tab_genus[1,1], " ", tab_genus$Freq[1], "/", sum(tab_genus$Freq), sep="")}
 
 # family
 tab_family <- as.data.frame(table(data_bold$family[data_bold$similarity>= MM[3]]), stringsAsFactors=F)
 tab_family <- tab_family[order(tab_family$Freq, decreasing=T),]
-adj_family <- paste(tab_family[1,1], " ", tab_family$Freq[1], "/", sum(tab_family$Freq), sep="")
+if(length(tab_family)==0){adj_family <- "NA"} else {adj_family <- paste(tab_family[1,1], " ", tab_family$Freq[1], "/", sum(tab_family$Freq), sep="")}
 
 # order
 tab_order <- as.data.frame(table(data_bold$order[data_bold$similarity>= MM[4]]), stringsAsFactors=F)
@@ -91,8 +97,9 @@ adj_order <- paste(tab_order[1,1], " ", tab_order$Freq[1], "/", sum(tab_order$Fr
 # add taxon name!
 
 new_taxonomy <- rbind(new_taxonomy, c(temp_files[i] , adj_order, adj_family, adj_genus, adj_species, data_bold$similarity[1]))
-
 }
+
+} # end loop
 
 new_taxonomy <- data.frame(new_taxonomy, stringsAsFactors=F)
 names(new_taxonomy) <- c("BOLD_OTU", "BOLD_Order", "BOLD_Family", "BOLD_Genus", "BOLD_Species", "BOLD_best_match")
