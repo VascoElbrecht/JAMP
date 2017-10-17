daf3c04d5e2dea0abc4160bc12762e3f3bdd9b29
Vasco Elbrecht <luckylion07@googlemail.com>
2017-10-17 19:49:59 -0400

    maps deactivate

--- JAMP/R/makemaps.R
+++ JAMP/R/makemaps.R
@@ -1,19 +1,19 @@
 # 171014 - make map plots
 
-makemaps <- function("shape file"){
+makemaps <- function(file="shape file"){
 
 
 
 }
 
 
-library("mapplots")
-library("sp")
+#library("mapplots")
+#library("sp")
 
-north <- readShapePoly("~/Documents/UNI_und_VORLESUNGEN/14 Guelph/1 Parks /0 raw data/1 Map ontario/0 shapefiles/Canada.shp")
+#north <- readShapePoly("~/Documents/UNI_und_VORLESUNGEN/14 Guelph/1 Parks /0 raw data/1 Map ontario/0 shapefiles/Canada.shp")
 
 
-install.packages("sp")
+#install.packages("sp")
 
 
 
