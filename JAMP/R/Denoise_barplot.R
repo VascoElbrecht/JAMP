457b7ca69954f62c87049f9d9a6dc46938971597
Vasco Elbrecht <luckylion07@googlemail.com>
2018-04-05 11:13:36 -0400

    added ladder

--- JAMP/R/Denoise_barplot.R
+++ JAMP/R/Denoise_barplot.R
@@ -15,7 +15,7 @@ colors <- c("#ffffff", "#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#
 
 head(data)
 #  make large plot showing haplotype compossition per sample
-
+names(data)[1] <- "OTU"
 OTUs <- unique(data$OTU)
 
 i <- 31
@@ -47,7 +47,30 @@ barplot(cbind(c(cols-sum(temp), sort(temp, decreasing=T))), col= colors, main = 
 barplot(cbind(sort(temp, decreasing=T)), col= colors[-1], main = sub("OTU_", "", OTUs[i]), xlab="", ylab="", yaxt="n", axisnames=F, cex.main=0.7)
 }
 
+
 }
+
+# ladder
+sequence <- c(0, seq(100, cols, 100))
+sequence <- sequence/cols*100
+spots <- data.frame("spot"=sequence, "lab"=0:(cols/100))
+
+if(nrow(spots)>10){
+spots <- spots[seq(1, nrow(spots), 5),]
+}
+if(nrow(spots)>10){
+spots <- spots[seq(1, nrow(spots), 5),]
+}
+if(nrow(spots)>10){
+spots <- spots[seq(1, nrow(spots), 5),]
+}
+
+# empty slot
+barplot(100, main = "ladder", xlab="", ylab="", yaxt="n", axisnames=F, cex.main=0.7, border=NA, col="white")
+
+axis(4, las=1, at= spots$spot, lab= spots$lab)
+# ladder end
+
 if(!is.na(out)){
 dev.off()
 }
