d017479102ba3f3eed0098207573945c7967afc4
Vasco Elbrecht <luckylion07@googlemail.com>
2018-09-23 16:00:28 -0400

    bug fixes
    * no longer prioritised NA as entries
    * cound data removed on taxon level

--- JAMP/R/BOLD_taxonomy.R
+++ JAMP/R/BOLD_taxonomy.R
@@ -2,18 +2,23 @@
 
 Bold_taxonomy <- function(file=NA, folder="", MM=c(0.98, 0.95, 0.90, 0.85)){
 
-oldwd <- getwd()
-setwd(folder)
-getwd()
 #cat(file="../log.txt", c("\n","Version v0.1", "\n"), append=T, sep="\n")
 message("Getting taxonomy via BOLD api.")
 
+# read table
 data <- read.csv(file, stringsAsFactors=F)
+csv <- T
+
+if(ncol(data)==1){ # fasta
+data <- read.fasta(file, forceDNAtolower=F, as.string=T)
+data <- data.frame("ID"=names(data), sequ=as.vector(unlist(data)), stringsAsFactors=F)
+scv<-F
+}
+
 sequ <- which(!is.na(data$sequ)) # sequences exist
 
 dir.create("_BOLD_data", showWarnings = FALSE)
-setwd("_BOLD_data")
-dir.create("_stats", showWarnings = FALSE)
+dir.create("_BOLD_data/_stats", showWarnings = FALSE)
 
 #based on ID_engine_bp_v2_anna_NovemberR.R
 # query sequences to bold API
@@ -24,7 +29,7 @@ sequ_inital_DL <- sequ
 
 # skipp if already downloaded!
 temp_files <- paste(data$ID[sequ_inital_DL], ".csv", sep="")
-sequ_inital_DL <- sequ_inital_DL[!temp_files%in%list.files("_stats", "csv")]
+sequ_inital_DL <- sequ_inital_DL[!temp_files%in%list.files("_BOLD_data/_stats", "csv")]
 
 for (i in sequ_inital_DL){
 
@@ -42,7 +47,7 @@ hittable <- hittable[[1]]
 hittable <- hittable[order(hittable$similarity, decreasing=T),]
 }
 
-write.table(hittable, file=paste("_stats/", data$ID[i], ".csv", collapse="", sep=""), sep="\t")
+write.table(hittable, file=paste("_BOLD_data/_stats/", data$ID[i], ".csv", collapse="", sep=""), sep="\t")
 message(i)
 
 }
@@ -51,11 +56,11 @@ message(i)
 new_taxonomy <- NULL
 for (i in 1:length(temp_files)){
 
-if(readLines(paste("_stats/", data$ID[i], ".csv", collapse="", sep=""))[1]=="\"\""){new_taxonomy <- rbind(new_taxonomy, c(temp_files[i] , NA, NA, NA, NA, NA, NA, NA))} else {#no hit
+if(readLines(paste("_BOLD_data/_stats/", data$ID[i], ".csv", collapse="", sep=""))[1]=="\"\""){new_taxonomy <- rbind(new_taxonomy, c(temp_files[i] , NA, NA, NA, NA, NA, NA, NA))} else {#no hit
 
 
 
-data_bold <- read.csv(paste("_stats/", temp_files[i], sep=""), sep="\t", stringsAsFactors=F)
+data_bold <- read.csv(paste("_BOLD_data/_stats/", temp_files[i], sep=""), sep="\t", stringsAsFactors=F)
 
 if(is.null(data_bold$family)){data_bold <- data.frame(data_bold, "family"=rep(NA, nrow(data_bold)))}
 if(is.null(data_bold$genus)){data_bold <- data.frame(data_bold, "genus"=rep(NA, nrow(data_bold)))}
@@ -75,22 +80,45 @@ data_bold[,4:7][is.na(data_bold[,4:7])] <- "NA"
 # species
 tab_species <- as.data.frame(table(data_bold$species[data_bold$similarity>= MM[1]]), stringsAsFactors=F)
 tab_species <- tab_species[order(tab_species$Freq, decreasing=T),]
-if(length(tab_species)==0){adj_species <- "NA"} else {adj_species <- paste(tab_species[1,1], " ", tab_species$Freq[1], "/", sum(tab_species$Freq), sep="")}
+if(length(tab_species)==0){adj_species <- "NA"} else {
+tab_species <- tab_species[!tab_species$Var1=="NA",]
+adj_species <- tab_species[1,1]
+}
 
 # genus
 tab_genus <- as.data.frame(table(data_bold$genus[data_bold$similarity>= MM[2]]), stringsAsFactors=F)
 tab_genus <- tab_genus[order(tab_genus$Freq, decreasing=T),]
-if(length(tab_genus)==0){adj_genus <- "NA"} else {adj_genus <- paste(tab_genus[1,1], " ", tab_genus$Freq[1], "/", sum(tab_genus$Freq), sep="")}
+if(length(tab_genus)==0){adj_genus <- "NA"} else {
+tab_genus <- tab_genus[! tab_genus$Var1=="NA",]
+adj_genus <- tab_genus[1,1]
+if(!is.null(nrow(tab_species))){
+if(nrow(tab_species)>0){
+tab_genus <- tab_species[1,]
+tab_genus$Var1 <- sub("(.*) .*", "\\1",tab_genus$Var1)
+adj_genus <- tab_genus[1,1]}
+}
+}
+
 
 # family
 tab_family <- as.data.frame(table(data_bold$family[data_bold$similarity>= MM[3]]), stringsAsFactors=F)
 tab_family <- tab_family[order(tab_family$Freq, decreasing=T),]
-if(length(tab_family)==0){adj_family <- "NA"} else {adj_family <- paste(tab_family[1,1], " ", tab_family$Freq[1], "/", sum(tab_family$Freq), sep="")}
+if(length(tab_family)==0){adj_family <- "NA"} else {
+
+tab_family <- tab_family[! tab_family $Var1=="NA",]
+adj_family <- tab_family[1,1]
+}
+
 
 # order
 tab_order <- as.data.frame(table(data_bold$order[data_bold$similarity>= MM[4]]), stringsAsFactors=F)
 tab_order <- tab_order[order(tab_order$Freq, decreasing=T),]
-if(length(tab_order)==0){adj_order <- "NA"} else {adj_order <- paste(tab_order[1,1], " ", tab_order$Freq[1], "/", sum(tab_order$Freq), sep="")}
+if(length(tab_order)==0){adj_order <- "NA"} else {
+tab_order <- tab_order[! tab_order $Var1=="NA",]
+adj_order <- tab_order[1,1]
+}
+
+
 
 # Add taxonomic level
 # add taxon name!
@@ -122,12 +150,13 @@ bold_taxonomy <- NA
 
 new_taxonomy <- data.frame(new_taxonomy, stringsAsFactors=F)
 names(new_taxonomy) <- c("BOLD_OTU", "BOLD_Order", "BOLD_Family", "BOLD_Genus", "BOLD_Species", "BOLD_best_match", "bold_taxon", "bold_level")
-new_taxonomy <- rbind(new_taxonomy, NA)
 
+if(csv){
+new_taxonomy <- rbind(new_taxonomy, NA)
+}
 
 export <- cbind(data, new_taxonomy)
 
-write.csv(export, file=paste("../", sub(".csv", "_BOLD.csv", file), sep=""), row.names=F)
+write.csv(export, file=paste("_BOLD_data/BOLD_API_taxonomy.csv", sep=""), row.names=F)
 
-setwd(oldwd)
 }
