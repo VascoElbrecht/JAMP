4b2e245a917600d36a02805e8362d5029e76d1cf
Vasco Elbrecht <luckylion07@googlemail.com>
2018-10-25 11:15:13 -0400

    fix reporting

--- JAMP/R/Demultiplexing_index.R
+++ JAMP/R/Demultiplexing_index.R
@@ -181,7 +181,7 @@ for (i in 1:length(temp3)){
 cat("", file=temp3[i], sep="")
 }
 
-text <- paste("Empty files where generated for ", length(temp)/2, " samples:\n", paste(temp3, collapse="\n"), sep="")
+text <- paste("Empty files where generated for ", length(temp3)/2, " samples:\n", paste(temp3, collapse="\n"), sep="")
 message(text)
 cat(file="log.txt", text, append=T, sep="")
 
