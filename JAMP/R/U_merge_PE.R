0098a28312572ee90fa377ee1fdf4e5832a91942
Vasco Elbrecht <luckylion07@googlemail.com>
2019-03-10 20:24:21 +0100

    bugfix PE

--- JAMP/R/U_merge_PE.R
+++ JAMP/R/U_merge_PE.R
@@ -58,7 +58,7 @@ message(" ")
 # new file names
 
 new_names <- sub(".*(/.*)", "\\1", file1)
-if(fastq){new_names <- sub("[rR]1.*", "PE.fastq", new_names)} else {new_names <- sub("[rR]1.*", "PE.fasta", new_names)}
+if(fastq){new_names <- sub("_[rR]1.*", "_PE.fastq", new_names)} else {new_names <- sub("_[rR]1.*", "_PE.fasta", new_names)}
 
 new_names <- paste(folder, "/_data", new_names, sep="")
 
