29a6b929c84bb2163a82f90d078506a37716c2b3
Vasco Elbrecht <luckylion07@googlemail.com>
2020-01-21 18:28:26 +0100

    up

--- JAMP/R/Denoise.R
+++ JAMP/R/Denoise.R
@@ -795,7 +795,7 @@ message(paste(sep="Haplotype distribution plot generated:\n", folder, "/_stats/H
 
 
 if(heatmap){
-OTU_heatmap(file=paste(folder, "/E_haplo_table.csv", sep=""),  stringsAsFactors=F), out=paste(folder, "/_stats/map_E_haplo_table.pdf", sep=""), abundance=T, col=rev(c("#d7191c", "#fdae61", "#ffffbf", "#abdda4", "#2b83ba")))
+OTU_heatmap(file=paste(folder, "/E_haplo_table.csv", sep=""), out=paste(folder, "/_stats/map_E_haplo_table.pdf", sep=""), abundance=T, col=rev(c("#d7191c", "#fdae61", "#ffffbf", "#abdda4", "#2b83ba")))
 
 
 }
