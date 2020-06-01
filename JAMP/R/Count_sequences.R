d96c39e00355844dc3674904da65364ea5115abc
Vasco Elbrecht <luckylion07@googlemail.com>
2020-06-01 15:39:32 +0200

    up - usearch fix

--- JAMP/R/Count_sequences.R
+++ JAMP/R/Count_sequences.R
@@ -4,14 +4,14 @@ Count_sequences <- function(files=NA, fastq=T, count_size=F){
 
 # add autodetect of fastq or not!
 
-
+empty <- file.info(files)$size==0
 
 if(!count_size){
 if(fastq){ #count fastq
 cmd <- paste("", files, " | wc -l", sep="")
 
-A <- NULL
-for (i in 1:length(files)){
+A <- rep(0, length(files))
+for (i in which(!empty)){
 A[i] <- system2("cat", cmd[i], stdout=T)
 }
 
@@ -23,8 +23,8 @@ if(!fastq){ # count fasta
 cmd <- paste(" -c \"^>\" \"", files, "\"", sep="")
 #cmd <- paste(" -o \"^>\" \"", files, "\" | wc -l", sep="") # old
 
-A <- NULL
-for (i in 1:length(files)){
+A <- rep(0, length(files))
+for (i in which(!empty)){
 A[i] <- system2("grep", cmd[i], stdout=T)
 }
 
@@ -36,17 +36,27 @@ abundance <- as.numeric(sub(" ", "", A))
 
 if(count_size){ # count fasta - size annotations
 
-A <- NULL
-for (i in 1:length(files)){
+A <- rep(0, length(files))
+for (i in which(!empty)){
 fasta_sequ <- readLines(files[i])
 fasta_sequ <- fasta_sequ[grep(">", fasta_sequ)]
-A[i] <- sub(".*;size=(.*);?", "\\1", fasta_sequ)
-A[i] <- sum(as.numeric(sub(";", "", A[i])))
+
+if(grepl(";$", fasta_sequ[1])){
+A[i] <- sum(as.numeric(sub(".*;size=(.*);", "\\1", fasta_sequ)))
+} else {
+A[i] <- sum(as.numeric(sub(".*;size=(.*)", "\\1", fasta_sequ)))
 }
 
-abundance <- as.numeric(sub(" ", "", A))
-
 }
 
+abundance <- A
+}
 return(abundance)
+
 }
+# 200601
+# Usearch
+# fasta_sequ <- "NAME;size=327;"
+#
+# Vsearch
+# fasta_sequ <- "NAME;size=327"
